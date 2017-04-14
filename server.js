b1b9172b531d387e68400019a3fa8b8b7c1b4234
Keith Matthews <keith@alice.caltech.edu>
2017-04-13 18:24:51 -0700

    Reverting back to WebSockets from express-ws.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 var express = require('express');
 var app = express();
-var expWs = require('express-ws')(app);
 
 // app.use(function(request, response, next) {
 //     console.log('middleware');
@@ -52,35 +51,36 @@ app.get('/socket.io.js', function(request, response, next) {
 });
 
 // WebSocket Portion
-// using express-ws now that we are converting over to using Express
-
-// Register a callback function to run when we have an individual connection.
-// This is run for each individual user that connects.
-app.ws('/socket.io/', function(ws, request) {
-    ws.on('connection',
-        // We are given a websocket object in our function
-        function(socket) {
-            console.log("We have a new client: " + socket.id);
-
-            // When this user emits, client side: socket.emit('otherevent', some data);
-            socket.on('mouse',
-                function(data) {
-                    // Data comes in as whatever was sent, including objects
-                    console.log("Received: 'mouse' " + data.x + " " + data.y);
-                    // Send it to all other clients
-                    socket.broadcast.emit('mouse', data);
-                    // This is a way to send to everyone, including sender
-                    // ws.emit('message', "this goes to everyone");
-                }
-            );
-
-            socket.on('disconnect', function() {
-                console.log("Client has diconnected");
-            });
-        }
-    );
-});
-
+// WebSockets work with the HTTP Server
+//   Putting back the WebSockets method using instructions in
+//   http://stackoverflow.com/questions/21365044/cant-get-socket-io-js
 var server = app.listen(8080, function() {
     console.log('Server started on port 8080');
 });
+var io = require('socket.io').listen(server);
+
+
+// Register a callback function to run when we have an individual connection.
+// This is run for each individual user that connects.
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function(socket) {
+        console.log("We have a new client: " + socket.id);
+
+        // When this user emits, client side: socket.emit('otherevent', some data);
+        socket.on('mouse',
+            function(data) {
+                // Data comes in as whatever was sent, including objects
+                console.log("Received: 'mouse' " + data.x + " " + data.y);
+                // Send it to all other clients
+                socket.broadcast.emit('mouse', data);
+                // This is a way to send to everyone, including sender
+                // io.sockets.emit('message', "this goes to everyone");
+            }
+        );
+
+        socket.on('disconnect', function() {
+            console.log("Client has disconnected");
+        });
+    }    
+);
