16dea18f7dc283f0401dd2691ff573fcc5bc49df
Keith Matthews <keith@alice.caltech.edu>
2017-05-02 14:22:23 -0700

    Moved and renamed the three callbacks for dragging an object in Raphael.  Started calling the thing to be dragged a tile rather than an object.

--- app.js
+++ app.js
@@ -18,30 +18,31 @@ window.onload = function() {
 
     var img = paper.image("http://i.imgur.com/L9uSTVr.png", 0, 0, 3024, 2160);
 
-    var obj = paper.rect(210, 225, 80, 50);
-    obj.attr({fill: '#000', stroke: 'none', cursor: "move"});
-
-    function move(dx, dy) {
-        this.attr({x: this.ox + dx, y: this.oy + dy});
-    }
-
-    function start() {
-        this.ox = this.attr("x");
-        this.oy = this.attr("y");
-    }
-
-    function end() {
-        // report new final position to server
-        sendposition(this.attr("x"), this.attr("y"));
-    }
-
-    obj.drag(move, start, end);
+    var tile = paper.rect(210, 225, 80, 50).attr({fill: '#000', 'fill-opacity': 0.5, stroke: 'none', cursor: "move"});
+    // for the currently selected tile
+    tile.attr({stroke: '#802', 'stroke-width': 3, 'stroke-opacity': 0.5});
+    tile.drag(ongoingDrag, onStartDrag, onEndDrag);
 }
 
+// ===============================================
+// Callbacks for dragging onscreen objects
+function ongoingDrag(dx, dy) {
+    this.attr({ x: this.ox + dx, y: this.oy + dy });
+}
+
+function onStartDrag() {
+    this.ox = this.attr("x");
+    this.oy = this.attr("y");
+}
+
+function onEndDrag() {
+    // report new final position to server
+    sendPosition(this.attr("x"), this.attr("y"));
+}
 
 // ===============================================
 // Function for sending through the socket
-function sendposition(xpos, ypos) {
+function sendPosition(xpos, ypos) {
     // We are sending!
     console.log("sendpos: " + xpos + " " + ypos);
 
