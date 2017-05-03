f59e29fc7729a336464f0c1016f428ec9977c72f
Keith Matthews <keith@alice.caltech.edu>
2017-05-02 17:05:18 -0700

    Now with 2 tiles in an array.  Implemented tile selection via a callback function attached to each tile object.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 // Establish socket connection to server
 var socket;
-
+var tiles = [];
+var selectedTile;
 
 window.onload = function() { 
     var paper = new Raphael(document.getElementById('canvas_container'), 500, 500);
@@ -18,14 +19,40 @@ window.onload = function() {
 
     var img = paper.image("http://i.imgur.com/L9uSTVr.png", 0, 0, 3024, 2160);
 
-    var tile = paper.rect(210, 225, 80, 50).attr({fill: '#000', 'fill-opacity': 0.5, stroke: 'none', cursor: "move"});
-    // for the currently selected tile
-    tile.attr({stroke: '#802', 'stroke-width': 3, 'stroke-opacity': 0.5});
-    tile.drag(ongoingDrag, onStartDrag, onEndDrag);
+    // tile 0
+    tiles.push(paper.rect(210, 200, 80, 50).attr({fill: '#000', 'fill-opacity': 0.5, stroke: 'none'}));
+    tiles[0].node.onclick = function() {
+        stealSelection(0, selectedTile);
+    };
+
+    // tile 1
+    tiles.push(paper.rect(210, 265, 80, 50).attr({fill: '#000', 'fill-opacity': 0.5, stroke: 'none'}));
+    tiles[1].node.onclick = function() {
+        stealSelection(1, selectedTile);
+    };
+
+    selectedTile = 0;
+    // stealSelection(0, selectedTile);
 }
 
 // ===============================================
-// Callbacks for dragging onscreen objects
+// OnClick callback making tiles draggable
+function stealSelection(thisTileNum, selectedTile) {
+    // remove highlight on previously selected tile
+    tiles[selectedTile].attr({ stroke: 'none', cursor: 'auto' });
+    // set undrag on previously selected tile
+    tiles[selectedTile].undrag();
+
+    // highlight tile with red stroke
+    tiles[thisTileNum].attr({ stroke: '#802', 'stroke-width': 3, 'stroke-opacity': 0.5, cursor: 'move' });
+    // make tile draggable
+    tiles[thisTileNum].drag(ongoingDrag, onStartDrag, onEndDrag);
+
+    selectedTile = thisTileNum;
+}
+
+// ===============================================
+// Callbacks for dragging tiles
 function ongoingDrag(dx, dy) {
     this.attr({ x: this.ox + dx, y: this.oy + dy });
 }
