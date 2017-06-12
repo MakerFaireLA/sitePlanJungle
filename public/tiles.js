6ca392769d7b74ee03e21256a909f6145b185803
Keith Matthews <keith@alice.caltech.edu>
2017-06-12 13:27:07 -0700

    Improved font size and made the line break work on the tile text annotation.

--- public/tiles.js
+++ public/tiles.js
@@ -7,7 +7,7 @@
 //   @TODO - Understand why this works for rerendering an already added tile.
 function renderTile(tile) {
     const html = `<div id="id-${tile.tile_id}" class="tile" style="left: ${Math.round(tile.x/scale_factor)}px; 
-        top: ${Math.round(tile.y/scale_factor)}px; transform: rotateZ(${tile.theta}deg);">${tile.userRef}EOL${tile.userLabel}</div>`;
+        top: ${Math.round(tile.y/scale_factor)}px; transform: rotateZ(${tile.theta}deg);">${tile.userRef}<br>${tile.userLabel}</div>`;
     $('.container').append(html);
 
     $('#id-' + tile.tile_id).mousedown(function(event) {
